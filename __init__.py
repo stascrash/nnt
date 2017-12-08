915555d2517c1751238b292d2522b1ae80b4a671
Stas Poritskiy <stascrash@gmail.com>
2017-12-08 21:53:10 +0100

    Integrating with fman

--- __init__.py
+++ __init__.py
@@ -1,13 +1,20 @@
+import sys
 import subprocess
 import os.path as path
+sys.path.append(path.dirname(__file__))
+
 from fman import ApplicationCommand, show_alert
-import core.commands
+
+import note_taker
 
 class Note(ApplicationCommand):
 	def __call__(self, *args, **kwargs):
-		sys_py_exe = r"C:\Python35-32\python.exe"
-		program_file = path.abspath(path.join(path.dirname(__file__), "note_taker.py"))
-		p = subprocess.run([sys_py_exe, program_file], shell=True)
-		exit_code = p.check_returncode()
-		if exit_code != 0:
-			show_alert("Something went wrong with NoteTaker. Exit Code: {}".format(exit_code))
+
+		nnt = note_taker.NoteController()
+		nnt.exec_()
+		# sys_py_exe = r"C:\Python35-32\python.exe"
+		# program_file = path.abspath(path.join(path.dirname(__file__), "note_taker.py"))
+		# p = subprocess.run([sys_py_exe, program_file], shell=True)
+		# exit_code = p.check_returncode()
+		# if exit_code != 0:
+		# 	show_alert("Something went wrong with NoteTaker. Exit Code: {}".format(exit_code))
